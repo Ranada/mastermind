acd21b55186cfb5e28e6a6d59395464192c7014b
Neil Ranada <neil.ranada@gmail.com>
2022-01-10 23:09:27 -0800

    Add check function checking c flag argument

--- functions_validations.c
+++ functions_validations.c
@@ -16,7 +16,7 @@ void check_arguments(int argc, char* argv[])
 void check_c_flag(int argc, char* argv[])
 {
     int i;
-    
+
     for (i = 0; i < argc; i++)
     {
         char first_char = argv[i][0];
@@ -24,7 +24,32 @@ void check_c_flag(int argc, char* argv[])
         
         if (first_char == '-' && second_char == 'c')
         {
-            printf("I am a C FLAG!");  
+            printf("I am a C FLAG!\n");
+            check_c_argument(argv, i);  
+        }
+    }
+}
+
+void check_c_argument(char* argv[], int i)
+{
+    char* c_flag_arg = argv[i + 1];
+    int c_flag_arg_length = strlen(c_flag_arg);
+    int j;
+
+    printf("C FLAG ARGUMENT: %s\n", c_flag_arg);
+    
+    if (c_flag_arg_length == CODE_LENGTH)
+    {
+        for (j = 0; j < c_flag_arg_length; j++)
+        {
+            char ch = c_flag_arg[j];
+            
+            if (ch > '0' && ch < '7')
+            {
+                printf("I CAN BE THE SECRET CODE: %s\n", c_flag_arg);
+                secret_code = c_flag_arg;
+                printf("I'M THE NEW SECRET CODE!: %s\n", secret_code);
+            }
         }
     }
 }
--- mastermind.h
+++ mastermind.h
@@ -3,19 +3,20 @@
 
 /* Libraries */
 #include <stdio.h>
+#include <string.h>
 
 /* MACROS */
 #define CODE_LENGTH 4
 
 /* Global Variables */
-char* possible_digits[8];
-char* secret_code[CODE_LENGTH];
-char* guess_code[CODE_LENGTH];
+char* secret_code;
+char* guess_code;
 int* attempts;
 
 /* Function prototypes */
 void play_mastermind(int argc, char* argv[]);
 void check_arguments(int argc, char* argv[]);
 void check_c_flag(int argc, char* argv[]);
+void check_c_argument(char* argv[], int i);
 
 #endif //MASTERMIND_H
